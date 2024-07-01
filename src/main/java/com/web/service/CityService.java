c83d0ef91ad84d827434e1320bf3ce5e1f9b278c
ketan <ketan.thombare@gmail.com>
2024-07-01 13:30:33 +0530

    wwwwwwwwwwwwwwwwwww

--- src/main/java/com/web/service/CityService.java
+++ src/main/java/com/web/service/CityService.java
@@ -7,7 +7,7 @@ public class CityService {
 
 	public String getCities() {
 		System.out.println("99999999999999999999999999");
-		return "Mumbai...,Vashi,......Thane...,Panvel,Ghansoli,Vikroli,kandivali,Rabale,vbbbbbashi,Airolimgt,Sanpada,pune,Nerul,Belapur,thane,pune,ghansoli";
+		return "Mumbai...,Vashi,......Thane...,Panvel....,Ghansoli,Vikroli,kandivali,Rabale,vbbbbbashi,Airolimgt,Sanpada,pune,Nerul,Belapur,thane,pune,ghansoli";
 	}
 	
 	
